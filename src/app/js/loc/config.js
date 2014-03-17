ecdb2e1a94198a25cd3b43f66f63ddf32cc0dd82
Jim Blaney <jblaney@esri.com>
2014-03-17 19:22:05 -0400

    updated feature service url

--- src/app/js/loc/config.js
+++ src/app/js/loc/config.js
@@ -10,7 +10,7 @@ dojoConfig.app = {
   selectionColor: [ 255, 255, 0 ],
   basemap: "gray",
   layers: {
-    states: "http://loc-cong-demo.esri.com/arcgis/rest/services/Congress/Congress113/MapServer/0",
-    districts: "http://loc-cong-demo.esri.com/arcgis/rest/services/Congress/Congress113/MapServer/1"
+    states: "http://legislator-demo.esri.com/arcgis/rest/services/Congress/Congress113/MapServer/0",
+    districts: "http://legislator-demo.esri.com/arcgis/rest/services/Congress/Congress113/MapServer/1"
   }
 }
